0a43f3fd806e8cfae53258a0a7e9ecef540abefc
Enzo Mello <enzobahiamello@gmail.com>
2026-02-26 20:32:21 +0000

    Adicionando estrutura inicial

---
+++ RandomGroupGeneratorLibrary/src/main/java/org/example/GroupGenerator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class GroupGenerator {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            String[] estudantes;
+        }
+    }
+}
